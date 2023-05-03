74e6a8eb3d3db9318697f686d9f7fa20300ddb88
Xavier Graells <xaviergraells@Xaviers-Air.lan>
2023-05-03 14:51:29 +0200

    added mouse down without delete of drawing

--- client/src/components/DrawingPad.js
+++ client/src/components/DrawingPad.js
@@ -20,43 +20,55 @@ const options = {
 };
 
 export default function DrawingPad({ selectedRoomId, selectedEventName }) {
-  const [points, setPoints] = React.useState([]);
-
-  function handlePointerDown(e) {
-    e.target.setPointerCapture(e.pointerId);
-    const rect = e.target.getBoundingClientRect();
-    setPoints([[e.clientX - rect.left, e.clientY - rect.top, e.pressure]]);
-  }
+    const [strokes, setStrokes] = React.useState([]);
+    const [currentStroke, setCurrentStroke] = React.useState([]);
   
-  function handlePointerMove(e) {
-    if (e.buttons !== 1) return;
-    const rect = e.target.getBoundingClientRect();
-    setPoints([...points, [e.clientX - rect.left, e.clientY - rect.top, e.pressure]]);
-  }
+    function handlePointerDown(e) {
+      e.target.setPointerCapture(e.pointerId);
+      const rect = e.target.getBoundingClientRect();
+      setCurrentStroke([[e.clientX - rect.left, e.clientY - rect.top, e.pressure]]);
+    }
   
-
-  const stroke = getStroke(points, options);
-  const pathData = getSvgPathFromStroke(stroke);
-
-  return (
-    <>
-      {selectedRoomId !== "todo" && (
-        <div className="drawing-pad-container">
-          <div className="drawing-pad-wrapper">
-            <span>Drawing pad:</span>
-            <svg
-              onPointerDown={handlePointerDown}
-              onPointerMove={handlePointerMove}
-              style={{ touchAction: "none" }}
-            >
-              {points && <path d={pathData} />}
-            </svg>
+    function handlePointerMove(e) {
+      if (e.buttons !== 1) return;
+      const rect = e.target.getBoundingClientRect();
+      setCurrentStroke([
+        ...currentStroke,
+        [e.clientX - rect.left, e.clientY - rect.top, e.pressure],
+      ]);
+    }
+  
+    function handlePointerUp(e) {
+      setStrokes([...strokes, currentStroke]);
+      setCurrentStroke([]);
+    }
+  
+    return (
+      <>
+        {selectedRoomId !== "todo" && (
+          <div className="drawing-pad-container">
+            <div className="drawing-pad-wrapper">
+              <span>Drawing pad:</span>
+              <svg
+                onPointerDown={handlePointerDown}
+                onPointerMove={handlePointerMove}
+                onPointerUp={handlePointerUp}
+                style={{ touchAction: "none" }}
+              >
+                {strokes.map((stroke, index) => (
+                  <path key={index} d={getSvgPathFromStroke(getStroke(stroke, options))} />
+                ))}
+                {currentStroke && (
+                  <path d={getSvgPathFromStroke(getStroke(currentStroke, options))} />
+                )}
+              </svg>
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
-}
+        )}
+      </>
+    );
+  }
+  
 
 function getSvgPathFromStroke(stroke) {
     if (!stroke.length) return ""
